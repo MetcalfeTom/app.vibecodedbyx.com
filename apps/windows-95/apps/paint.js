7914823dc3fcac954ac97b4b8ed3e7d6978a1361
sloppy <sloppy@vibecodedbyx.com>
2025-09-28 12:49:55 +0000

    windows-95: avoid global name collision by renaming paint instance to windows95Paint and updating initializer

--- apps/windows-95/apps/paint.js
+++ apps/windows-95/apps/paint.js
@@ -117,14 +117,14 @@ class Paint {
     }
 }
 
-// Global paint instance
-let paint;
+// Global paint instance (scoped to avoid collisions)
+let windows95Paint;
 
 // Global functions
 function selectPaintTool(tool) {
-    if (paint) paint.selectTool(tool);
+    if (windows95Paint) windows95Paint.selectTool(tool);
 }
 
 function clearPaintCanvas() {
-    if (paint) paint.clearCanvas();
+    if (windows95Paint) windows95Paint.clearCanvas();
 }
